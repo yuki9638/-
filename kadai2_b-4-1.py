15f6d81310de7bb4b140ab249053bbd17bfda386
LAPTOP-T3VI0BNC\yurur <yu_ki9638@icloud.com>
2023-10-11 15:44:43 +0900

    練習問題b-4-1の問題を解きそのファイルを作成しました

--- kadai2_b-4-1.py
+++ kadai2_b-4-1.py
@@ -5,7 +5,14 @@ pyxel.cls(7)
 for a in range(10, 200, 20):
  for b in range(10,200,20) :
     if ((a-10)/20) % 2 == 0:
+     if ((b-10)/20)%2 == 0 :
         pyxel.circ(a, b, 10, 6)
-    else:
+     else :
+       pyxel.circ(a,b,10,14)      
+    elif ((b-10)/20)%2 == 0 : 
+     if ((a-10)/20)%2 != 0 :  
         pyxel.circ(a, b, 10, 14)
+    else :
+      pyxel.circ(a, b, 10, 6)
 pyxel.show()
+# 縦の行と横の行の円を個数の単位に還元し、その数字の奇遇で場合分けをしました
