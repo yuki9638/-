a8c12208de08d391057e7b603927a3b10e764535
LAPTOP-T3VI0BNC\yurur <yu_ki9638@icloud.com>
2023-11-01 16:11:02 +0900

    kadai_B-10-1を追加しました

--- kadai_B-9-2.py
+++ kadai_B-9-2.py
@@ -10,7 +10,9 @@ vy = pyxel.sin(angle)
 a= 0
 speed =1.5
 padx = 100
+pyxel.sound(0).set("e0e1c1","p","3","s",10)
 
+pyxel.sound(1).set("a2c3","t","7","v",50)
 def update():
     global ballx, bally, vx, vy,padx,a
     ballx += vx*speed
@@ -20,13 +22,16 @@ def update():
         ballx = pyxel.rndi(0,199)
         bally = 0
         vx=vx*speed
-        vy=vy*speed
+        vy=vy
     if ballx >=200 :
         vx=-0.866
     if ballx<=0 :
         vx=0.866
     if bally>=195 and padx-20<=ballx<=padx+20 :
         a+=1
+        pyxel.play(0, [0,0])
+    if bally>=195 and (padx-20>ballx or padx+20<ballx) :
+        pyxel.play(1, [1,1])
 def draw():
     global ballx, bally, vx, vy, padx,a
     pyxel.cls(7)
